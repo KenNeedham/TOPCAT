5dec76c41cde0a1aa87e427c224dffaec5ac5f0a
Ken Needham <ken.needham@gmail.com>
2018-08-16 09:56:00 +0100

    Status page updated

--- src/components/status.jsx
+++ src/components/status.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { getStatuses } from "./../services/fakeMetadataService";
+import Table from "./common/table";
 
-const Status = () => {
-  return <h1>Status</h1>;
-};
+class Status extends Component {
+  state = {
+    statuses: [],
+    currentPage: 1,
+    pageSize: 4,
+    searchQuery: "",
+    sortColumn: { path: "updated", order: "desc" }
+  };
+
+  columns = [
+    {
+      path: "sma",
+      label: "SMA"
+    },
+    { path: "ssma", label: "SSMA" },
+    { path: "dataset", label: "Dataset" },
+    { path: "updated", label: "Updated" }
+  ];
+
+  render() {
+    return (
+      <div>
+        {" "}
+        <Table
+          columns={this.columns}
+          data={this.statuses}
+          sortColumn={this.sortColumn}
+          onSort={this.onSort}
+        />
+      </div>
+    );
+  }
+}
 
 export default Status;
